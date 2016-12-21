d08a191ab90b2cd99d82fe2d35412d4742a6799f
vanessamnoble <vanessamnoble@icloud.com>
2016-12-21 11:42:13 -0600

    final div slider upon click event frame

--- personal_page/Personal_Page.js
+++ personal_page/Personal_Page.js
@@ -1,3 +1,59 @@
 /**
  * Created by vanessamnoble on 12/20/16.
  */
+
+//Sticky Navbar
+window.onscroll = function() {sticky_nav()};
+function sticky_nav() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    {
+        document.getElementById("nav").className = 'sticky';
+    } else {   document.getElementById("nav").className = '';}
+}
+
+
+function validateForm() {
+    var x = document.forms["form"]["name"].value;
+    var y = document.forms["form"]["message"].value;
+    if ((x == null || x == "")&&(y == null || y == "")){
+        document.getElementById('messages').innerHTML = "Please enter a name and a message";
+        return false;
+    } else if (y == null || y == "") {
+        document.getElementById('messages').innerHTML = "Please enter a message";
+        return false;
+    } else if (x == null || x == "") {
+        document.getElementById('messages').innerHTML = "Please enter a name";
+        return false;
+    } else {
+        alert ("Thank You!");
+    }
+}
+
+
+
+// div slider
+$( document ).ready(function() {
+    $( '#my-slider' ).sliderPro();
+});
+$( document ).ready(function() {
+        $( '#example1' ).sliderPro({
+        width: 960,
+        height: 500,
+        responsive: true,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        thumbnailWidth: 200,
+        thumbnailHeight: 100,
+        thumbnailPointer: true,
+        autoplay: false,
+        autoScaleLayers: true,
+        breakpoints: {
+            500: {
+                thumbnailWidth: 120,
+                thumbnailHeight: 50
+            }
+        }
+    });
+}); //end div slider
+
