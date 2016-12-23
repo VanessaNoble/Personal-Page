a4da11959d19d4cc8aeb75111066d159ed4d838f
vanessamnoble <vanessamnoble@icloud.com>
2016-12-22 22:46:23 -0600

    added a new header section

--- personal_page/statsSection.js
+++ personal_page/statsSection.js
@@ -22,12 +22,12 @@ var data = {
         },]
 };
 
-var chartWidth       = 300,
-    barHeight        = 20,
+var chartWidth       = 230,
+    barHeight        = 15,
     groupHeight      = barHeight * data.series.length,
     gapBetweenGroups = 10,
-    spaceForLabels   = 150,
-    spaceForLegend   = 150;
+    spaceForLabels   = 110,
+    spaceForLegend   = 90;
 
 // Zip the series data together (first values, second values, etc.)
 var zippedData = [];
@@ -124,91 +124,42 @@ legend.append('text')
     .attr('y', legendRectSize - legendSpacing)
     .text(function (d) { return d.label; });
 
-// //line graph
-// var svg = d3.select("svg"),
-//     margin = {top: 20, right: 80, bottom: 30, left: 50},
-//     width = svg.attr("width") - margin.left - margin.right,
-//     height = svg.attr("height") - margin.top - margin.bottom,
-//     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-// var parseTime = d3.timeParse("%Y%m%d");
-//
-// var x = d3.scaleTime().range([0, width]),
-//     y = d3.scaleLinear().range([height, 0]),
-//     z = d3.scaleOrdinal(d3.schemeCategory10);
-//
-// var line = d3.line()
-//     .curve(d3.curveBasis)
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.temperature); });
-//
-// d3.tsv("data.tsv", type, function(error, data) {
-//     if (error) throw error;
-//
-//     var cities = data.columns.slice(1).map(function(id) {
-//         return {
-//             id: id,
-//             values: data.map(function(d) {
-//                 return {date: d.date, temperature: d[id]};
-//             })
-//         };
-//     });
-//
-//     x.domain(d3.extent(data, function(d) { return d.date; }));
-//
-//     y.domain([
-//         d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
-//         d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
-//     ]);
-//
-//     z.domain(cities.map(function(c) { return c.id; }));
-//
-//     g.append("g")
-//         .attr("class", "axis axis--x")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(d3.axisBottom(x));
-//
-//     g.append("g")
-//         .attr("class", "axis axis--y")
-//         .call(d3.axisLeft(y))
-//         .append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("y", 6)
-//         .attr("dy", "0.71em")
-//         .attr("fill", "#000")
-//         .text("Temperature, ºF");
-//
-//     var city = g.selectAll(".city")
-//         .data(cities)
-//         .enter().append("g")
-//         .attr("class", "city");
-//
-//     city.append("path")
-//         .attr("class", "line")
-//         .attr("d", function(d) { return line(d.values); })
-//         .style("stroke", function(d) { return z(d.id); });
-//
-//     city.append("text")
-//         .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-//         .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-//         .attr("x", 3)
-//         .attr("dy", "0.35em")
-//         .style("font", "10px sans-serif")
-//         .text(function(d) { return d.id; });
-// });
-//
-// function type(d, _, columns) {
-//     d.date = parseTime(d.date);
-//     for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
-//     return d;
-// }
 
-//pie chart
+//google graphs
 
 
+// Load Charts and the corechart and barchart packages.
+google.charts.load('current', {'packages':['corechart']});
 
+// Draw the pie chart and bar chart when Charts is loaded.
+google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
 
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+    ]);
+
+    var piechart_options = {title:'Pie Chart: How Much Pizza I Ate Last Night',
+        width:420,
+        height:450};
+    var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
+    piechart.draw(data, piechart_options);
+
+    var barchart_options = {title:'Barchart: How Much Pizza I Ate Last Night',
+        width:420,
+        height:450,
+        legend: 'none'};
+    var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
+    barchart.draw(data, barchart_options);
+}
 
 
 
